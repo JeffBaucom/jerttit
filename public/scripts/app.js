266010d48fa7021b9650a9a5f650fcc52ebb7f03
JeffBaucom <jeffbaucom@gmail.com>
2016-05-04 18:41:21 -0400

    setting up frontend stuff

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	angular.module('jerttit', [
+			'ui.router',
+			'ui.bootstrap',
+			'ngMaterial'
+
+
+
+
+		]);
+})();
---
+++ public/scripts/routes.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+	angular.module ('jerttit')
+	.config (['$stateProvider', '$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+
+		}]);
+})();
