b186ddb84068d54e67e125a9e90d2a944021771c
JeffBaucom <jeffbaucom@gmail.com>
2016-05-11 18:40:30 -0700

    fix api routing for jerts
    needs consolidating and refactoring

--- server.js
+++ server.js
@@ -17,28 +17,66 @@ app.use(function(req, res, next) {
 app.use(morgan('dev'));
 
 
-var app = express();
 var db;
 var users;
-var articles;
+var posts;
+var jerts;
 
 var MONGODB_URI = "mongodb://localhost:27017/jerttit";
 
-mongodb.MongoClient.connect(MONGODB_URI, function(err, database) {
-  if(err) throw err;
- 
-  db = database;
-  users = db.collection('users');
-  articles = db.collection('articles');
+var connectDB = function() {
+  mongodb.MongoClient.connect(MONGODB_URI, function(err, database) {
+    if(err) throw err;
+   
+    db = database;
+    users = db.collection('users');
+    posts = db.collection('posts');
+    jerts = db.collection('jerts');
 
-  app.listen(3000);
-  console.log('Listening on port 3000');
-});
+    
+  });
+};
+connectDB();
+app.listen(3000);
+console.log('Listening on port 3000');
 
 app.use(express.static(__dirname + '/public'));
 
+//var jertsRoutes = require('./app/routes/jerts')(app, express, jerts);
+//app.use('/api/jerts', jertsRoutes);
 
-app.get('*', function(req, res) {
+  app.get('/api/jerts', function(req, res) {
+      jerts.find({}).toArray(function(err, docs) {
+        if (err) res.send(err);
+        res.json(docs);
+          //db.close();
+      });
+
+  });
+
+  app.post('/api/jerts', function(req, res) {
+      var newJert;
+
+      console.log(req.body);
+      if (req.body.name && req.body.description && req.body.url) {
+        newjert = {
+          name: req.body.name,
+          description: req.body.description,
+          url: req.body.url
+        };
+
+        jerts.insert(newjert, function(err, result) {
+          if (err) res.send(err);
+          console.log("new Jert inserted");
+        });
+
+        db.close();
+        connectDB();
+      }
+
+  });
+
+app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
