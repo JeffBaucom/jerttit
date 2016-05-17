a33f8c84585372b80bdc2bfe80fd5da992299fe6
JeffBaucom <jeffbaucom@gmail.com>
2016-05-16 19:20:28 -0700

    add Jerts Service to front-end

--- public/scripts/services/JertsService.js
+++ public/scripts/services/JertsService.js
@@ -5,6 +5,34 @@
 	Jerts.$inject = ['$http'];
 
 	function Jerts($http) {
+
+		return {
+			getJerts : getJerts,
+			getJert : getJert,
+			postJert : postJert
+		};
+
+		function getJerts() {
+			return $http.get('api/jerts/')
+				.then(getJertsComplete)
+				.catch(getJertsFailed);
+
+			function getJertsComplete(response) {
+				return response.data.results;
+			}
+
+			function getJertsFailed(error) {
+				console.log(error.data);
+			}
+		}
+
+		function getJert(url) {
+
+		}
+
+		function postJert(newJert) {
+			return $http.post('api/jerts/')
+		}
 		
 	}
 })();
